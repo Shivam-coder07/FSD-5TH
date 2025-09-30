d873155202b93820847b4a46e3f891c42a73eae4
shivam yadav <shivamyadav0631@gmail.com>
2025-09-30 14:59:34 +0530

    done

--- day-9/promise.js
+++ day-9/promise.js
@@ -9,12 +9,22 @@ const wait = (msg,sec)=>{
     },sec);
     });
 }
-const bookTicket=()=>{
+// const bookTicket=()=>{
+//     printMessage("Welcome");
+//     wait("Login Sucessfully",3000)
+//     .then(()=>wait("seat has been selected sucessfully",2000))
+//     .then(()=>wait("your seat has been booked",3000))
+//     .then(()=>wait("your seat has been booked",4000))
+//     .then(()=>wait("Ticket has been confirmed",3000))
+//     .then(()=>printMessage("Thankyou"))
+// }
+
+
+const bookTicket=async()=>{
     printMessage("Welcome");
-    wait("Login Sucessfully",3000)
-    .then(()=>wait("seat has been selected sucessfully",2000))
-    .then(()=>wait("your seat has been booked",3000))
-    .then(()=>wait("your seat has been booked",4000))
-    .then(()=>wait("Ticket has been confirmed",3000))
-    .then(()=>printMessage("Thankyou"))
+    await wait("Login Sucessfully",3000);
+    await wait("seat has been selected sucessfully",4000);
+    await wait("your seat has been booked",3000);
+    await wait("Payment has been done",4000);
+    await wait("Ticket has been confirmed",3000);
 }
